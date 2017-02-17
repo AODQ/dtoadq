3d74f72447443bcd2b9ebf1f4132d1955fb2ceec
aodq <aodqig@gmail.com>
2017-02-17 11:47:56 -0500

    opencl stuff

--- source/app.d
+++ source/app.d
@@ -16,40 +16,7 @@ void OpenCL_Test ( ) {
 
   Initialize;
   Print_Device_Information;
-
-
-
-
-  // import opencl;
-  // import functional;
-  // auto someKernelDef = CLKernelDef!("someKernel",
-  //         CLBuffer!(float), "input",
-  //         float, "b")
-  // (q{
-  //     input[gLinId] = exp(cos(input[gLinId] * b));
-  // });
-
-  // auto platform = getChosenPlatform();
-
-  // auto devices = platform.getDevices(cl.DEVICE_TYPE_GPU);
-  // auto context = createContext(devices);
-  // auto queue = context.createCommandQueue(devices[0]);
-  // auto kernel = context.createProgram(someKernelDef)
-  //     .buildProgram
-  //     .createKernel!(someKernelDef.name); // 
-
-  // auto input = iota(90).map!(to!float).array;
-
-  // auto buff = context
-  //     .newBuffer(cl.MEM_COPY_HOST_PTR, input);
-
-  // kernel.setArgs(buff, 1.4f);
-  // queue.enqueueCLKernel(kernel, [10UL, 9UL]);
-
-  // auto output = new float[](90);
-  // queue.read(buff, output);
-
-  // writeln("OUTPUT: ", output);
+  Test_OpenCL;
 }
 
 void main() {
--- source/opencl.d
+++ source/opencl.d
@@ -5,6 +5,10 @@ public static import wrap = clwrap.clwrap;
 import clwrap.clwrap, std.stdio;
 import std.conv : to;
 
+void OpenCLAssert(bool condition, string error) {
+  assert(condition, error);
+}
+
 auto RPlatforms() {
   platform_id[] platforms;
   cl_uint platform_amt = 10;
@@ -12,6 +16,61 @@ auto RPlatforms() {
   getPlatformIDs(platform_amt, platforms.ptr, &platform_amt);
   platforms.length = platform_amt;
   return platforms;
+}
+
+struct Device {
+  cl_platform_id platform_id;
+  cl_device_id   device_id;
+}
+
+auto RPlatform() {
+  auto platforms = RPlatforms;
+  OpenCLAssert(platforms.length >= 1, "No OpenCL platform found");
+  cl_platform_id platform_id = cast(void*)platforms[0];
+  cl_device_id device_id;
+  writeln("GETTING device ID stuff");
+  clGetDeviceIDs(&platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, null);
+  writeln("PLATFORM ID: ", platform_id);
+  writeln("DEVICE ID: ", device_id);
+  return Device(platform_id, device_id);
+}
+
+struct CLProgram {
+  cl_context context;
+  cl_context_properties[] properties;
+  cl_command_queue command_queue;
+  cl_program program;
+  cl_device_id device_id;
+  cl_platform_id platform_id;
+  cl_kernel kernel;
+}
+
+auto Compile_Program ( string source, Device device) {
+  cl_context_properties[] properties = [
+    CL_CONTEXT_PLATFORM,
+    cast(cl_context_properties) device.platform_id,
+    0
+  ];
+
+  auto context =
+    clCreateContext(properties.ptr, 1, &device.device_id, null, null, null);
+
+  auto command_queue = clCreateCommandQueue(context, device.device_id, 0, null);
+
+  import std.string;
+  auto e = source.toStringz;
+  auto program = clCreateProgramWithSource(context, 1, &e, null, null);
+  OpenCLAssert(
+    clBuildProgram(program, 0, null, null, null, null) != CL_SUCCESS,
+    "Error building program");
+  return CLProgram(
+    context, properties, command_queue, program,
+    device.device_id, device.platform_id
+  );
+}
+
+auto Compile(string source) {
+  return Compile_Program(source, RPlatform);
 }
 
 alias PlatformID = platform_id;
@@ -45,7 +104,7 @@ auto RPlatform_Info(PlatformID platform) {
 }
 
 void Print_Device_Information ( ) {
-  auto platforms = RPlatforms
+  auto platforms = RPlatforms;
   writeln("PLATFORMS: ", platforms);
   import std.algorithm;
   platforms.each!(n => n.RPlatform_Info.writeln);
@@ -54,4 +113,54 @@ void Print_Device_Information ( ) {
 
 void Initialize ( ) {
   DerelictCL.load();
+}
+
+
+void Test_OpenCL ( ) {
+  float[10] input_data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+  auto program = Compile(`
+    __kernel void hello(__global float* input, __global float* output) {
+      size_t id = get_global_id(0);
+      output[id] = input[id] * input[id];
+    }
+  `);
+
+  import std.string;
+  auto e = "hello".toStringz;
+  auto size = float.sizeof*input_data.length;
+  auto kernel = clCreateKernel(program.program, "hello", null);
+  auto input  = clCreateBuffer(program.context, CL_MEM_READ_ONLY,
+                    size, null, null);
+  auto output = clCreateBuffer(program.context, CL_MEM_WRITE_ONLY,
+                    size, null, null);
+
+  // load data into input buffer
+  clEnqueueWriteBuffer(program.command_queue, input, CL_TRUE, 0,
+                    size, input_data.ptr, 0, null, null);
+  // set arg list for kernel command
+  clSetKernelArg(kernel, 0, cl_mem.sizeof, &input);
+  clSetKernelArg(kernel, 1, cl_mem.sizeof, &output);
+  size_t global = input_data.length;
+
+  // enqueue kernel command for execution
+  clEnqueueNDRangeKernel(program.command_queue, kernel, 1, null, &global, null,
+                         0, null, null);
+  clFinish(program.command_queue);
+
+  // copy results from out of output buffer
+  float[10] results;
+  clEnqueueReadBuffer(program.command_queue, output, CL_TRUE, 0, size,
+                      results.ptr, 0, null, null);
+  writeln("Output: ");
+  import std.algorithm, std.range;
+  results.each!writeln;
+
+  // cleanup opencl resources
+  clReleaseMemObject(input);
+  clReleaseMemObject(output);
+  clReleaseProgram(program.program);
+  clReleaseKernel(kernel);
+  clReleaseCommandQueue(program.command_queue);
+  clReleaseContext(program.context);
 }
